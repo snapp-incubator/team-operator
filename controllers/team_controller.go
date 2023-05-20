81518290a95dd9697fd1efafb0cf8c77d23d412f
navid.shariaty <navid.shariaty@snapp.cab>
2023-05-20 18:37:35 +0330

    change suffix to team

--- controllers/team_controller.go
+++ controllers/team_controller.go
@@ -33,7 +33,7 @@ import (
 )
 
 const (
-	MetricNamespaceSuffix    = "-metrics"
+	MetricNamespaceSuffix    = "-team"
 	MetricNamespaceFinalizer = "batch.finalizers.kubebuilder.io/metric-namespace"
 )
 
